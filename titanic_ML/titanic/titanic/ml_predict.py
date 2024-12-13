c128549b26f52ca08cb218fd5c227f9f8786faa4
binubims <binubimshara2015@gmail.com>
2024-12-13 00:51:51 -0800

    after fixing django files so ubuntu can host the djangosite

--- titanic_ML/titanic/titanic/ml_predict.py
+++ titanic_ML/titanic/titanic/ml_predict.py
@@ -1,8 +1,18 @@
 
+import os
+import pickle
+
+# Construct the path dynamically
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model_path = os.path.join(BASE_DIR, "titanic_model.sav")
+
+# Load the model
+with open(model_path, 'rb') as file:
+    randomforest = pickle.load(file)
+
 def prediction_model(pclass,sex,age,sibsp,parch,fare,embarked,title):
-    import pickle
     x=[[pclass,sex,age,sibsp,parch,fare,embarked,title]]
-    randomforest = pickle.load(open('titanic_model.sav','rb'))
+    # randomforest = pickle.load(open('titanic_model.sav','rb'))
     prediction = randomforest.predict(x)
     if prediction == 0:
         return "not survived"
